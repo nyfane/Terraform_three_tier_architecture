72ac9d31a8da0afefe191d349b7214327420635c
Fanny G <99718866+nyfane@users.noreply.github.com>
2022-06-14 00:48:30 -0600

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
-    bucket = "statefiles3bucket"
-    key    = "network/terraform.tfstate"
-    region = "us-west-2"
-    dynamo_table = "my-dynamo-statefile"
-  }
-}
